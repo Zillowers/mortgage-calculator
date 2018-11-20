ed1a301234e43f3c381cdef832c3336901f9ed6a
htlin2 <htlin2@gmail.com>
2018-11-19 19:59:19 -0800

    connected to ec2 mongoDB

--- database/mongoDB/index.js
+++ database/mongoDB/index.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+const { dbKey } = require('./dbKey.js');
 
-const mongoUri = 'mongodb://localhost/zillower';
+const mongoUri = `mongodb://${dbKey}@db.htlin.io:27017/zillower`;
+// const mongoUri = 'mongodb://localhost/zillower';
 const db = mongoose.connect(mongoUri);
 
 module.exports = db;
--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('newrelic');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -5,7 +6,7 @@ const port = 3000;
 // const { retrieve } = require('./../database/dbMethods.js'); //MySql
 const { retrieve } = require('../database/postgreSQL'); // postgreSQL
 const db = require('../database/mongoDB');
-const { Mortgages } = require('../database/mongoDB/mortgageDB.js');
+const { Mortgages } = require('../database/mongoDB/MortgageDB.js');
 
 const app = express();
 
@@ -17,20 +18,19 @@ app.listen(port, () => {
   console.log(`listening at ${port}`);
 });
 
-
 app.use('/homes/:id', express.static('public/dist'));
 
-// app.get('/api/homes/:id/prices', (req, res) => {
-//   const { id } = req.params;
-//   retrieve(id, (err, data) => {
-//     if (err) {
-//       res.end(err);
-//     } else {
-//       res.writeHead(200, { 'Content-Type': 'application/json' });
-//       res.end(JSON.stringify(data));
-//     }
-//   });
-// });
+app.get('/api/postgres/:id', (req, res) => {
+  const { id } = req.params;
+  retrieve(id, (err, data) => {
+    if (err) {
+      res.end(err);
+    } else {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(data));
+    }
+  });
+});
 
 // MongoDB
 app.get('/api/homes/:id/prices', (req, res) => {
