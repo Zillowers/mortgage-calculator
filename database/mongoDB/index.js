7e67e49da6e6d665a3fa6e9b1e7ac0e9a766b70c
htlin2 <htlin2@gmail.com>
2018-11-21 16:10:38 -0800

    port updated to 80

--- database/mongoDB/index.js
+++ database/mongoDB/index.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 // const autoIncrement = require('mongoose-auto-increment');
 const { dbKey } = require('./dbKey.js');
 
-// const mongoUri = `mongodb://${dbKey}@db1.htlin.io:27017/zillower`;
-const mongoUri = 'mongodb://localhost/zillower';
+const mongoUri = `mongodb://${dbKey}@db1.htlin.io:27017/zillower`;
+// const mongoUri = 'mongodb://localhost/zillower';
 const db = mongoose.connect(mongoUri, { useNewUrlParser: true, useCreateIndex: true });
 // const connection = mongoose.createConnection(mongoUri, { useNewUrlParser: true, useCreateIndex: true });
 // autoIncrement.initialize(connection);
